d1181112b25b810e46d50e8d716d9442e9589e10
Kevin <kh216@students.waikato.ac.nz>
2022-06-13 21:28:11 +1200

    Initial

---
+++ app/models/projects.model.js
@@ -0,0 +1,11 @@
+const db = require("./db");
+
+// TODO: complete the code as per the instructions given in Assignment 4
+
+
+const Project = function(/* ... */) {
+  // ...
+};
+
+
+module.exports = Project;
