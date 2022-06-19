e8613036b4f70f5dbac921dc920de006478c70b8
Kevin <kh216@students.waikato.ac.nz>
2022-06-19 18:03:13 +1200

    Update

--- app/models/projects.model.js
+++ app/models/projects.model.js
@@ -41,7 +41,7 @@ Project.getProjectAll = (resultCallback) => {
 
 //Retrieve Project By ID
 Project.getProjectID = (pID,resultCallback) => {
-  var sql = "SELECT * FROM projects WHERE ID=`" + pID + "`";
+  var sql = "SELECT * FROM projects WHERE ID=" + pID;
   db.query(sql, function(err, res) {
     if (err) {
       console.log('Error: ', err);
@@ -86,7 +86,7 @@ Project.updateProjectID = (pid, project, resultCallback) => {
 //Delete Projects By ID
 Project.removeProjectID = (pID,resultCallback) => {
     
-  var sql = "DELETE * FROM `" + TableName + "` WHERE ID=`" + pID + "`";
+  var sql = "DELETE * FROM `" + TableName + "` WHERE ID=" + pID;
   db.query(sql, function(err, res) {
     if (err) {
       console.log('Error: ', err);
